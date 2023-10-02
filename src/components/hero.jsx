5148337f8e451ceea6cdb54c05207f3690b72f73
AnshumanDhiman <anshumandhiman1@gmail.com>
2023-10-02 19:11:24 +0530

    fixes

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,22 +1,39 @@
 import heroImg from "../assets/working.svg";
 import Card from "./cards";
-import Cards from './cards'
 
-function createCard(card_data) {
-  return (
-    <Card
-      title={card_data.title}
-      info={card_data.info}
-      color={card_data.color}
-    />
-  );
-}
 
 export default function Hero() {
   return (
     <>
       <div className="relative grid w-full bg-gradient-to-r from-blue-500 to-blue-800 h-[25rem] items-center">
-        <div className="flex flex-col items-center text-center">
+      <div className="px-28 py-8">
+          <div className="items-center flex">
+            <div className="w-full lg:w-1/2">
+              <h1 className="text-4xl font-bold text-[#F5F5F5]  ">
+                Managed IT Solutions for
+                <br />
+                growing your<span className="text-white"> Business</span>
+              </h1>
+
+              <p className="mt-3 text-white">
+              We build secure Cloud Solutions that target your business needs
+              </p>
+
+              {/* <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+                Contact Us
+              </button> */}
+            </div>
+
+            <div className="flex items-center justify-center w-full mt-0 lg:w-1/2">
+              <img
+                className="w-[500px] h-full lg:max-w-2xl"
+                src={heroImg}
+                alt="image showing working culture"
+              />
+            </div>
+          </div>
+        </div>
+        {/* <div className="flex flex-col items-center text-center">
           <h1 className="text-6xl font-semibold text-white uppercase">
             We are CWR
           </h1>
@@ -24,10 +41,10 @@ export default function Hero() {
           <p className="mt-6 text-lg leading-5 text-white">
             Lorem ipsum dolor sit amet.
           </p>
-        </div>
+        </div> */}
       </div>
 
-      <header className="bg-white">
+      {/* <header className="bg-white">
         <div className="px-28 py-20">
           <div className="items-center flex">
             <div className="w-full lg:w-1/2">
@@ -57,7 +74,7 @@ export default function Hero() {
             </div>
           </div>
         </div>
-      </header>
+      </header> */}
 
       <Card/>
       
