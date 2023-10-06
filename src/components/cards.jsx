7fd3042eddbadf8cfb058d4a78777cd7747a06fa
AnshumanDhiman <anshumandhiman1@gmail.com>
2023-10-07 00:18:56 +0530

    new changes

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 // border-[${props.color}]
 import {AiOutlineArrowRight} from 'react-icons/ai'
+import {BsArrowRight} from 'react-icons/bs'
 
 function Card() {
   return (
@@ -11,29 +12,31 @@ function Card() {
         className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out hover:scale-110 border-t-[16px] border-[#00C0E8] "
       >
         <h1 className="text-2xl font-bold bg-white text-left">Roadmap to Better Decision-Making</h1>
-        <p className="py-4 bg-white text-left">Get the right information at the Right time to enable your business to take the Right decision.</p>
-        <AiOutlineArrowRight className=' relative left-[45%]'/>
+        <p className="py-4 bg-white text-left">Get the right information at the Right time to enable your business to take the Right decision.
+        </p>
+        <BsArrowRight className='relative top-[6%] text-2xl left-[85%] text-[#3366CC] '/>
       </div>
       <div
         className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out hover:scale-110 border-t-[16px] border-[#EA1179] "
       >
         <h1 className="text-2xl font-bold bg-white text-left">Industry experts in Cloud Solutions</h1>
         <p className="py-4 bg-white text-left">Hands-on experience with diverse Industries, Geographies and new Technologies up to date.</p>
-        <AiOutlineArrowRight className=' relative left-[45%]'/>
+        <BsArrowRight className='relative top-[6%]  text-2xl left-[85%] text-[#3366CC] '/>
       </div>
       <div
         className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out hover:scale-110 border-t-[16px] border-[#9400FF] "
       >
         <h1 className="text-2xl font-bold bg-white text-left">Industry experts in Cybersecurity</h1>
         <p className="py-4 bg-white text-left">Vast experience in complex business environments with diverse industries and their solutions.</p>
-        <AiOutlineArrowRight className=' relative left-[45%]'/>
+        
+        <BsArrowRight className='relative top-[6%]  text-2xl left-[85%] text-[#3366CC] '/>
       </div>
       <div
         className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out hover:scale-110 border-t-[16px] border-[#92D050] "
       >
         <h1 className="text-2xl font-bold bg-white text-left">Roadmap to Better Decision-Making</h1>
         <p className="py-4 bg-white text-left">Get the right information at the Right time to enable your business to take the Right decision.</p>
-        <AiOutlineArrowRight className=' relative left-[45%]'/>
+        <BsArrowRight className='relative top-[6%]  text-2xl left-[85%] text-[#3366CC] '/>
       </div>
       </div>
     
--- src/pages/services/Software.jsx
+++ src/pages/services/Software.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import heroImg from "../../assets/software.svg";
+import {BsArrowRight} from 'react-icons/bs'
 export default function Software() {
   return (
     <>
@@ -32,72 +33,72 @@ export default function Software() {
         </div>
         <div className="grid grid-cols-4 px-32 gap-8 py-16 mt-20">
       <div
-        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out border-t-[16px] border-rose-700 "
+        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out border-t-[16px] hover:scale-110 border-[#00C0E8]"
       >
         <h1 className="text-2xl font-bold bg-white text-justifyt">Software Development</h1>
        
-        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <button className='text-sm px-4 py-2 font-sm text-white capitalize transition-colors duration-300 transform bg-rose-700 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80'>Explore More</button>
+        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <BsArrowRight className='relative top-[6%] h-[25px] w-[25px] left-[85%] text-[#3366CC] '/>
       </div>
       <div
-        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out border-t-[16px] border-rose-700 "
+        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out border-t-[16px] hover:scale-110 border-[#EA1179]  "
       >
         <h1 className="text-2xl font-bold bg-white text-left">Web Designing and Development</h1>
         
-        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <button className='text-sm px-4 py-2 font-sm text-white capitalize transition-colors duration-300 transform bg-rose-700 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80'>Explore More</button>
+        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <BsArrowRight className='relative top-[6%] h-[25px] w-[25px] left-[85%] text-[#3366CC] '/>
       </div>
       <div
-        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] border-rose-700"
+        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] hover:scale-110 border-[#9400FF]"
       >
         <h1 className="text-2xl font-bold bg-white text-left">Mobile App development</h1>
       
-        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <button className='text-sm px-4 py-2 font-sm text-white capitalize transition-colors duration-300 transform bg-rose-700 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80'>Explore More</button>
+        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <BsArrowRight className='relative top-[6%] h-[25px] w-[25px] left-[85%] text-[#3366CC] '/>
       </div>
       <div
-        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] border-rose-700"
+        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] hover:scale-110 border-[#92D050]"
       >
         <h1 className="text-2xl font-bold bg-white text-left">UI / UX Designing</h1>
         <br/> 
-        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <button className='text-sm px-4 py-2 font-sm text-white capitalize transition-colors duration-300 transform bg-rose-700 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80'>Explore More</button>
+        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <BsArrowRight className='relative top-[6%] h-[25px] w-[25px] left-[85%] text-[#3366CC] '/>
       
       </div>
       <div
-        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] border-rose-700"
+        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] hover:scale-110 border-[#00C0E8] mt-12"
       >
         <h1 className="text-2xl font-bold bg-white text-left">WordPress Website Development</h1>
         
-        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <button className='text-sm px-4 py-2 font-sm text-white capitalize transition-colors duration-300 transform bg-rose-700 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80'>Explore More</button>
+        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <BsArrowRight className='relative top-[6%] h-[25px] w-[25px] left-[85%] text-[#3366CC] '/>
       
       </div>
       <div
-        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] border-rose-700"
+        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] hover:scale-110 border-[#EA1179] mt-12"
       >
         <h1 className="text-2xl font-bold bg-white text-left">Web Hosting Services</h1>
 
-        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <button className='text-sm px-4 py-2 font-sm text-white capitalize transition-colors duration-300 transform bg-rose-700 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80'>Explore More</button>
+        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <BsArrowRight className='relative top-[6%] h-[25px] w-[25px] left-[85%] text-[#3366CC] '/>
       
       </div>
       <div
-        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] border-rose-700"
+        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] hover:scale-110 border-[#9400FF] mt-12"
       >
         <h1 className="text-2xl font-bold bg-white text-left">SEO Services</h1>
         <br/> 
-        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <button className='text-sm px-4 py-2 font-sm text-white capitalize transition-colors duration-300 transform bg-rose-700 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80'>Explore More</button>
+        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <BsArrowRight className='relative top-[6%] h-[25px] w-[25px] left-[85%] text-[#3366CC] '/>
       
       </div>
       <div
-        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] border-rose-700"
+        className="p-8 rounded-2xl shadow-xl bg-white cursor-pointer transition ease-in-out  border-t-[16px] hover:scale-110 border-[#92D050] mt-12"
       >
         <h1 className="text-2xl font-bold bg-white text-left">Quality Assurance (QA) Testing</h1>
        
-        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        <button className='text-sm px-4 py-2 font-sm text-white capitalize transition-colors duration-300 transform bg-rose-700 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80'>Explore More</button>
+        <p className="py-4 bg-white text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <BsArrowRight className='relative top-[6%] h-[25px] w-[25px] left-[85%] text-[#3366CC] '/>
       
       </div>
       </div>
