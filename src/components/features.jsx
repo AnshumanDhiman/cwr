ef32c937dc94b51b7137c72d30dcbd7f815f4504
AnshumanDhiman <anshumandhiman1@gmail.com>
2023-10-11 16:42:23 +0530

    new additions & fixes

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,160 +1,160 @@
 export default function Features() {
   return (
     <>
-      <section className="bg-white px-28 py-4">
+      <section className="bg-white px-28 py-4 mb-20">
         <div className="container px-6 py-10 mx-auto">
           <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl ">
-            explore our top <span className=" text-blue-500 ">Features</span>
+            Our top <span className=" text-blue-500 ">Features</span>
           </h1>
-
-          <p className="mt-4 text-gray-500 xl:mt-6 ">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum
-            quam voluptatibus
-          </p>
 
           <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-16 md:grid-cols-2 xl:grid-cols-3">
             <div className="space-y-3">
               <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full ">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
                   stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 640 512"
+                  height="24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
+                  <path d="M434.7 64h-85.9c-8 0-15.7 3-21.6 8.4l-98.3 90c-.1.1-.2.3-.3.4-16.6 15.6-16.3 40.5-2.1 56 12.7 13.9 39.4 17.6 56.1 2.7.1-.1.3-.1.4-.2l79.9-73.2c6.5-5.9 16.7-5.5 22.6 1 6 6.5 5.5 16.6-1 22.6l-26.1 23.9L504 313.8c2.9 2.4 5.5 5 7.9 7.7V128l-54.6-54.6c-5.9-6-14.1-9.4-22.6-9.4zM544 128.2v223.9c0 17.7 14.3 32 32 32h64V128.2h-96zm48 223.9c-8.8 0-16-7.2-16-16s7.2-16 16-16 16 7.2 16 16-7.2 16-16 16zM0 384h64c17.7 0 32-14.3 32-32V128.2H0V384zm48-63.9c8.8 0 16 7.2 16 16s-7.2 16-16 16-16-7.2-16-16c0-8.9 7.2-16 16-16zm435.9 18.6L334.6 217.5l-30 27.5c-29.7 27.1-75.2 24.5-101.7-4.4-26.9-29.4-24.8-74.9 4.4-101.7L289.1 64h-83.8c-8.5 0-16.6 3.4-22.6 9.4L128 128v223.9h18.3l90.5 81.9c27.4 22.3 67.7 18.1 90-9.3l.2-.2 17.9 15.5c15.9 13 39.4 10.5 52.3-5.4l31.4-38.6 5.4 4.4c13.7 11.1 33.9 9.1 45-4.7l9.5-11.7c11.2-13.8 9.1-33.9-4.6-45.1z"></path>
                 </svg>
               </span>
 
               <h1 className="text-xl font-semibold text-gray-700 capitalize ">
-                Expert Services Directory
+                Collaboration
               </h1>
 
               <p className="text-gray-500 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident ab nulla quod dignissimos vel non corrupti doloribus
-                voluptatum eveniet
+                We believe in collaboration and work closely with our clients to
+                achieve success.
               </p>
             </div>
 
             <div className="space-y-3">
               <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full ">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
                   stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 512 512"
+                  height="24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+                  <path d="M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm92.49,313h0l-20,25a16,16,0,0,1-22.49,2.5h0l-67-49.72a40,40,0,0,1-15-31.23V112a16,16,0,0,1,16-16h32a16,16,0,0,1,16,16V256l58,42.5A16,16,0,0,1,348.49,321Z"></path>
                 </svg>
               </span>
 
               <h1 className="text-xl font-semibold text-gray-700 capitalize ">
-                Client Testimonials and Success Stories
+                Reliability
               </h1>
 
               <p className="text-gray-500 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident ab nulla quod dignissimos vel non corrupti doloribus
-                voluptatum eveniet
+                We prioritize reliability and ensure consistent delivery of
+                quality solutions.
               </p>
             </div>
 
             <div className="space-y-3">
               <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full ">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
                   stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 576 512"
+                  height="24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+                  <path d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"></path>
                 </svg>
               </span>
 
               <h1 className="text-xl font-semibold text-gray-700 capitalize ">
-                Personalized Consultation Request
+                Transparency
               </h1>
 
               <p className="text-gray-500 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident ab nulla quod dignissimos vel non corrupti doloribus
-                voluptatum eveniet
+                We value transparency and maintain open communication throughout
+                the project lifecycle.
               </p>
             </div>
 
             <div className="space-y-3">
               <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full ">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
                   stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 512 512"
+                  height="24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
-                  <path d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
+                  <path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"></path>
                 </svg>
               </span>
 
               <h1 className="text-xl font-semibold text-gray-700 capitalize ">
-                Consultant Matching Algorithm
+                Quality Driven
               </h1>
 
               <p className="text-gray-500 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident ab nulla quod dignissimos vel non corrupti doloribus
-                voluptatum eveniet
+                We are committed to delivering high-quality solutions that
+                exceed client expectations.
               </p>
             </div>
 
             <div className="space-y-3">
               <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full ">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
                   stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 512 512"
+                  height="24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z" />
+                  <path d="M487.4 315.7l-42.6-24.6c4.3-23.2 4.3-47 0-70.2l42.6-24.6c4.9-2.8 7.1-8.6 5.5-14-11.1-35.6-30-67.8-54.7-94.6-3.8-4.1-10-5.1-14.8-2.3L380.8 110c-17.9-15.4-38.5-27.3-60.8-35.1V25.8c0-5.6-3.9-10.5-9.4-11.7-36.7-8.2-74.3-7.8-109.2 0-5.5 1.2-9.4 6.1-9.4 11.7V75c-22.2 7.9-42.8 19.8-60.8 35.1L88.7 85.5c-4.9-2.8-11-1.9-14.8 2.3-24.7 26.7-43.6 58.9-54.7 94.6-1.7 5.4.6 11.2 5.5 14L67.3 221c-4.3 23.2-4.3 47 0 70.2l-42.6 24.6c-4.9 2.8-7.1 8.6-5.5 14 11.1 35.6 30 67.8 54.7 94.6 3.8 4.1 10 5.1 14.8 2.3l42.6-24.6c17.9 15.4 38.5 27.3 60.8 35.1v49.2c0 5.6 3.9 10.5 9.4 11.7 36.7 8.2 74.3 7.8 109.2 0 5.5-1.2 9.4-6.1 9.4-11.7v-49.2c22.2-7.9 42.8-19.8 60.8-35.1l42.6 24.6c4.9 2.8 11 1.9 14.8-2.3 24.7-26.7 43.6-58.9 54.7-94.6 1.5-5.5-.7-11.3-5.6-14.1zM256 336c-44.1 0-80-35.9-80-80s35.9-80 80-80 80 35.9 80 80-35.9 80-80 80z"></path>
                 </svg>
               </span>
 
               <h1 className="text-xl font-semibold text-gray-700 capitalize ">
-                Client Dashboard and Progress Tracking
+                Process
               </h1>
 
               <p className="text-gray-500 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident ab nulla quod dignissimos vel non corrupti doloribus
-                voluptatum eveniet
+                We follow streamlined processes to ensure efficiency and
+                effective project management.
               </p>
             </div>
 
             <div className="space-y-3">
               <span className="inline-block p-3 text-blue-500 bg-blue-100 rounded-full ">
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
                   stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 352 512"
+                  height="24"
+                  width="24"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
+                  <path d="M96.06 454.35c.01 6.29 1.87 12.45 5.36 17.69l17.09 25.69a31.99 31.99 0 0 0 26.64 14.28h61.71a31.99 31.99 0 0 0 26.64-14.28l17.09-25.69a31.989 31.989 0 0 0 5.36-17.69l.04-38.35H96.01l.05 38.35zM0 176c0 44.37 16.45 84.85 43.56 115.78 16.52 18.85 42.36 58.23 52.21 91.45.04.26.07.52.11.78h160.24c.04-.26.07-.51.11-.78 9.85-33.22 35.69-72.6 52.21-91.45C335.55 260.85 352 220.37 352 176 352 78.61 272.91-.3 175.45 0 73.44.31 0 82.97 0 176zm176-80c-44.11 0-80 35.89-80 80 0 8.84-7.16 16-16 16s-16-7.16-16-16c0-61.76 50.24-112 112-112 8.84 0 16 7.16 16 16s-7.16 16-16 16z"></path>
                 </svg>
               </span>
 
               <h1 className="text-xl font-semibold text-gray-700 capitalize ">
-                Secure Data and Confidentiality Assurance
+                Innovation
               </h1>
 
               <p className="text-gray-500 ">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Provident ab nulla quod dignissimos vel non corrupti doloribus
-                voluptatum eveniet
+                We foster innovation and leverage cutting-edge technologies to
+                drive business growth.
               </p>
             </div>
           </div>
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -30,33 +30,30 @@ export default function Hero() {
           </div>
         </div>
       </div>
-      <div className=" grid grid-cols-2">
-        <p className="pl-32 pt-8 bg-white text-justify text-gray-600">
+      <p className="py-8 px-32 text-lg bg-white text-center text-gray-600  tracking-wider">
           We provide end to end Digital Transformation Consultancy Services to
           evolve your business and achieve agility. Every core team member has
           20+ years of experience in Information Technology with diverse
           industries and geography. We will take you to the cloud to meet your
           agility goals with lower cost, secure and scalable solutions and take
-          away your day-to-day IT challenges.
-          <br />
-          <br />
-          <span className="text-2xl font-bold">
-            What does agility mean to your business?
-          </span>
-          <br />
+          away your day-to-day IT challenges.</p>
+      <div className=" grid grid-cols-2 place-items-center">
+      <div className="pl-32">
+        <h1 className="text-3xl font-bold">What does agility mean to your business?</h1><br />
+          <p className="bg-white text-justify text-gray-600 tracking-wider">
           If technology is not agile then we can’t have an agile business.
           Become responsive to customers/user needs, production needs,
           provisioning rapidly and bring down delivery of product and services
           from months to hours. Develop new services faster, imitate your
           competitor faster, and speed with higher quality. We can achieve
           agility by adopting Cloud Computing Solutions e.g., New Technology
-          setup in Cloud, Cloud Migration, SaaS Business Solutions .
-        </p>
+          setup in Cloud, Cloud Migration, SaaS Business Solutions . </p>
+          </div>
         <div className="w-full px-32 py-12 text-center">
           <img src={HeroImg} />
         </div>
       </div>
-      <h1 class="pl-32 pt-8 text-2xl font-bold text-gray-600">
+      <h1 class="pl-32 pt-8 text-2xl font-bold text-gray-700">
         Roadmap to better Decision-Making
       </h1>
       <p class="pl-32 bg-white text-justify text-gray-600">
